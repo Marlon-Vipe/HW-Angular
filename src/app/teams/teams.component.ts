7db619d7cf5318aee346d4ab79f5d30df2b33ebe
Marlon Villalona <iamvillalona@gmail.com>
2024-09-01 17:01:53 -0400

    teams component added

---
+++ src/app/teams/teams.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teams',
+  standalone: true,
+  imports: [],
+  templateUrl: './teams.component.html',
+  styleUrl: './teams.component.css'
+})
+export class TeamsComponent {
+  teams = [
+  {
+    id: 1,
+    name: 'Tigres del Licey'
+  },
+  {
+    id: 2,
+    name: 'Aguilas Cibaeñas'
+  },
+  {
+    id: 3,
+    name: 'Leones del Escogido'
+  },
+  {
+    id: 4,
+    name: 'Gigantes del Cibao'
+  },
+  {
+    id: 5,
+    name: 'Toros del Este'
+  },
+  {
+    id: 6,
+    name: 'Estrellas Orientales'
+  },
+}
