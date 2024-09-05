375d8e4d900fee55ebe0e5a8f4174cbad5be16a1
Marlon Villalona <iamvillalona@gmail.com>
2024-09-04 22:41:44 -0400

    nav-bar added in appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { TeamsComponent } from './teams/teams.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({  // Este componente envuelve a todos.
   selector: 'app-root', // Forma en que se refiere a este componente en otro sitio.
   standalone: true, // Si queremos usar un modulo dentro de este componente, se usa en los {imports}
-  imports: [RouterOutlet, UserComponent,TeamsComponent],  // Aqui es que se usa (se tiene que colocar cada componente a utilizar para que pueda tener acceso)
+  imports: [RouterOutlet, UserComponent,TeamsComponent,NavBarComponent],  // Aqui es que se usa (se tiene que colocar cada componente a utilizar para que pueda tener acceso)
   templateUrl: './app.component.html', // Esto es lo que renderiza
   styleUrl: './app.component.css',// Esto es lo que renderiza tambien
 })
